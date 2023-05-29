ba56c527ee1619d9b479c377534ace419aab7f0b
Manasvi <103330308+manasvi83@users.noreply.github.com>
2023-05-29 23:36:39 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './components/Todo'
+import "./App.css"
+
+
+export default function App() {
+    return (
+        <>
+            <div className='main'>
+                <Todo />
+            </div>
+        </>
+    )
+}
